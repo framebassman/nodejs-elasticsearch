1ddae88aa0274e34262545e9efb8e6d975942b56
Dmitry Romashov <framebassman@gmail.com>
2025-03-31 12:19:57 +0200

    Reproduce issue

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { HelloWorld } from './hello-world';
-import { log } from './winston-elasticsearch-logger';
+// import { log } from './winston-elasticsearch-logger';
+import { log } from './elasticsearch-transport-logger';
 
 export const sayHello = () => {
   log.info('hello world');
