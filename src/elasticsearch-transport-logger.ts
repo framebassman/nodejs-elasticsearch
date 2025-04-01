e2eb9ba3c06ed50a88bbb6c49bdc3ccb7114232d
Dmitry Romashov <framebassman@gmail.com>
2025-04-01 08:07:36 +0200

    Fix

--- src/elasticsearch-transport-logger.ts
+++ src/elasticsearch-transport-logger.ts
@@ -1,9 +1,16 @@
-import winston from 'winston'
+import winston from 'winston';
 
-import TransportElastic from 'framebassman-elasticsearch-transport'
+import TransportElastic from 'framebassman-elasticsearch-transport';
+
+let dateString = new Date(new Date()).toISOString().split('T')[0];
+dateString = dateString.replaceAll('-', '.');
 
 export const log = winston.createLogger({
-  format: winston.format.combine(winston.format.timestamp(), winston.format.json(), winston.format.prettyPrint()),
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json(),
+    winston.format.prettyPrint()
+  ),
   transports: [
     new winston.transports.Console(),
     new TransportElastic({
@@ -12,13 +19,13 @@ export const log = winston.createLogger({
         node: 'https://kolenka-inc-4135333449.eu-central-1.bonsaisearch.net:443',
         auth: {
           username: 'NX4jPVtxmC',
-          password: 'QNw5bzyHoXC9YFkr',
+          password: 'QNw5bzyHoXC9YFkr'
         },
         headers: {
-          "Content-type": "application/json",
+          'Content-type': 'application/json'
         }
       },
-      index: 'filebeat-7.10.2'
+      index: `filebeat-7.10.2-${dateString}`
     })
-  ],
+  ]
 });
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,10 @@ import { HelloWorld } from './hello-world';
 import { log } from './elasticsearch-transport-logger';
 
 export const sayHello = () => {
+  let dateString = new Date(new Date()).toISOString().split('T')[0];
+  dateString = dateString.replaceAll('-', '.');
   log.info('hello world');
+  log.info(`Time now: ${dateString}`);
   return HelloWorld;
 };
 
